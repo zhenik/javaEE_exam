531518e575620f342ea25f49a97302874a2ca657
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-06-07 14:49:22 +0200

    update restaurant ejb -> add more functionality

--- backend/src/main/java/ejb/RestaurantEJB.java
+++ backend/src/main/java/ejb/RestaurantEJB.java
@@ -1,13 +1,14 @@
 package ejb;
 
 import entity.Dish;
+import entity.Menu;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.io.Serializable;
-import java.util.List;
+import java.util.*;
 
 @Stateless
 public class RestaurantEJB implements Serializable {
@@ -39,4 +40,50 @@ public class RestaurantEJB implements Serializable {
         List<Dish> dishes = query.getResultList();
         return dishes;
     }
+
+    public boolean createMenu(Date date, Dish... dishes){
+        if (date==null || dishes.length<1){
+            return false;
+        }
+
+        Set<Dish> menuDishes = new HashSet<>();
+        for (Dish dish:dishes){
+            Dish dishFromDb = getDish(dish.getName());
+            if (dishFromDb!=null){
+                menuDishes.add(dishFromDb);
+            }
+            else{
+                throw new IllegalArgumentException("Dish with name: "+dish.getName()+ " does not exists in database");
+            }
+        }
+        Date date1 = getFirstSecondOfDay(date);
+        Menu menu = new Menu();
+        menu.setDateId(date1);
+        menu.setDishes(menuDishes);
+        em.persist(menu);
+        return true;
+    }
+
+    // I fetch first millisecond (beginning of the day)
+    // the given date and persist it as dateId
+    private Date getFirstSecondOfDay(Date date){
+        Calendar c = GregorianCalendar.getInstance();
+        c.clear();
+        c.setTime(date);
+
+        int year = c.get(Calendar.YEAR);
+        int day = c.get(Calendar.DAY_OF_YEAR);
+        // need to clear day milliseconds
+        c.clear();
+        c.set(Calendar.DAY_OF_YEAR, day);
+        c.set(Calendar.YEAR, year);
+
+        return c.getTime();
+    }
+
+    public Menu getMenu(Date date){
+        Date date1 = getFirstSecondOfDay(date);
+        return em.find(Menu.class,date1);
+    }
+
 }
