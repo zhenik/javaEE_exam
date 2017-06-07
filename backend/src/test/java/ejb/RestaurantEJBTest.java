ee6ee2b328b546325ab9cf270dde734e3fe3d7c4
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-06-07 13:38:38 +0200

    add tests for restaurant ejb

---
+++ backend/src/test/java/ejb/RestaurantEJBTest.java
@@ -0,0 +1,103 @@
+package ejb;
+
+import entity.Dish;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJBException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class RestaurantEJBTest extends EjbTestBase  {
+
+    @Test
+    public void testCanCreateADish(){
+        //Arrange
+        String dishName = "dishName";
+        String dishDescription = "dishDescription";
+
+        //Act
+        boolean created = restaurantEJB.createDish(dishName,dishDescription);
+
+        //Assert
+        assertTrue(created);
+    }
+
+    @Test
+    public void testGetDish(){
+        //Arrange
+        String dishName = "dishName";
+        String dishDescription = "dishDescription";
+        boolean created = restaurantEJB.createDish(dishName,dishDescription);
+        assertTrue(created);
+
+        //Act
+        Dish dish1 = restaurantEJB.getDish(dishName);
+        Dish dish2 = restaurantEJB.getDish("not exist");
+
+        //Assert
+        assertNotNull(dish1);
+        assertNull(dish2);
+        assertEquals(dishName, dish1.getName());
+        assertEquals(dishDescription, dish1.getDescription());
+    }
+
+    @Test
+    public void testCreateDish_dishAlreadyExists(){
+        //Arrange
+        String dishName = "dishName";
+        String dishDescription = "dishDescription";
+
+        //Act
+        boolean created1 = restaurantEJB.createDish(dishName,dishDescription);
+        boolean created2 = restaurantEJB.createDish(dishName,dishDescription);
+
+        //Assert
+        assertTrue(created1);
+        assertFalse(created2);
+    }
+
+    @Test
+    public void testCreateADishWithWrongId(){
+
+        String dishName = "      ";
+        String dishDescription = "dishDescription";
+
+        // expect exception (entity dish has constraint)
+        try {
+            restaurantEJB.createDish(dishName,dishDescription);
+            fail();
+        }catch (EJBException e){}
+
+    }
+
+    @Test
+    public void testCreateADishWithEmptyDescription(){
+
+        String dishName = "name    ";
+        String dishDescription = "";
+
+        // expect false
+        if (restaurantEJB.createDish(dishName,dishDescription))
+            fail();
+    }
+
+    @Test
+    public void testGetAllDishes(){
+
+        // Arrange
+        boolean b1 = restaurantEJB.createDish("fish", "taste fish");
+        boolean b2 = restaurantEJB.createDish("meat", "taste meat");
+
+        // Act
+        List<Dish> dishes = restaurantEJB.getDishes();
+
+        // Assert
+        assertTrue(b1 && b2);
+        assertEquals(2, dishes.size());
+
+    }
+}
