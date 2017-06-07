724a5f7ae59c4cacf0723e86d626931bd9ea8be8
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-06-07 13:52:46 +0200

    add menu constraints size

--- backend/src/main/java/entity/Menu.java
+++ backend/src/main/java/entity/Menu.java
@@ -2,6 +2,7 @@ package entity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,6 +19,7 @@ public class Menu {
     @Column(unique = true)
     private Date date;
 
+    @Size(min = 1)
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Dish> dishes;
 
