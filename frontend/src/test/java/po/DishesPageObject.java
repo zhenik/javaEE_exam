748d832d93a6433e309e4cb49ff4e2878dffc759
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-06-08 11:36:45 +0200

    update dishes page object for tests

--- frontend/src/test/java/po/DishesPageObject.java
+++ frontend/src/test/java/po/DishesPageObject.java
@@ -1,6 +1,11 @@
 package po;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,5 +24,25 @@ public class DishesPageObject extends PageObject {
     @Override
     public boolean isOnPage() {
         return driver.getTitle().equals("Dishes");
+    }
+
+    public DishesPageObject createDish(String nameId){
+        setText("createDishForm:dishName",nameId);
+        setText("createDishForm:createText","description");
+
+
+        driver.findElement(By.id("createDishForm:createButton")).click();
+        waitForPageToLoad();
+        return this;
+    }
+
+    public boolean isDishExists(String dishName){
+        WebElement element = null;
+        try {
+            element = driver.findElement(
+//                    By.xpath("//table[@id='projectTable']//tbody//tr/td[contains(.,'"+dishName+"')]"));
+                    By.xpath("//table[@id='dishTable']//tbody//tr/td[contains(.,'"+dishName+"')]"));
+        }catch (NoSuchElementException e){}
+        return element!=null;
     }
 }
